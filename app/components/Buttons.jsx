dd5e8d99dea9eafcae732098e7bafc71f0e6416f
Koniesjay <jkonyeri@gmail.com>
2024-05-07 15:05:57 +0100

    first commit

--- app/components/Buttons.jsx
+++ app/components/Buttons.jsx
@@ -3,7 +3,10 @@ import React from "react";
 
 const Buttons = ({ text }) => {
   return (
-    <Link className="relative w-full cursor-pointer group" href="/wallet">
+    <Link
+      className="relative w-full cursor-pointer group"
+      href="/wallet/connecting"
+    >
       <div className="group-hover:hidden h-10 bg-[#1C1D20] -mb-11 ml-1 left-1 rounded-lg w-full"></div>
       <div className="rounded-lg bg-[#FFED76] font-semibold px-8 py-1.5 border-[2px] border-[#1C1D20] z-50">
         {text}
